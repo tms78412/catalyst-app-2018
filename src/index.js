02356820bc1d143b94da8b1e8a7d14e16a5850d4
TaylorS <taylors@lionsoft.net>
2018-09-07 13:58:47 -0500

    Add bootstrap npm package and import styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Local dependencies
 import './Css/index.css';
