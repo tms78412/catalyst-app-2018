481932e7290ce9626b33acf3dfabb83c84fb92b4
tms78412 <taylor@axomfoundry.com>
2018-09-07 15:46:15 -0500

    Add comments to History

--- src/History.js
+++ src/History.js
@@ -2,6 +2,14 @@
 import { createBrowserHistory } from 'history';
 
 //Instantiated
+/**
+ * This file simply uses the createBrowserHistory method exported from the history npm package to create and
+ * export a variable that can be used across our app
+ * 
+ * This allows us to reference the same history object throughout the project by including this file whenever history is needed
+ * 
+ * History is mounted to the Router object in index.js and keeps track of user navigation throughout the app
+ */
 const History = createBrowserHistory();
 
 export { History };
