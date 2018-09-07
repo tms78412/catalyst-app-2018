d6a9dab6f95164f3dcccd4a48b23560b58b124cd
TaylorS <taylors@lionsoft.net>
2018-09-07 13:59:34 -0500

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Local dependencies
-import './Css/index.css';
 import App from './App';
 import History from './History';
 import registerServiceWorker from './registerServiceWorker';
