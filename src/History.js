eb81342a207353bed342db65c781c19a5616a67b
tms78412 <taylor@axomfoundry.com>
2018-09-10 15:58:49 -0500

    Add reference information in comments of History

--- src/History.js
+++ src/History.js
@@ -6,7 +6,10 @@ import { createBrowserHistory } from 'history';
  * This file simply uses the createBrowserHistory method exported from the history npm package to create and
  * export a variable that can be used across our app
  * 
- * This allows us to reference the same history object throughout the project by including this file whenever history is needed
+ * Documentation and reference information for the history package can be found here:
+ * https://github.com/ReactTraining/history#readme
+ * 
+ * This file allows us to reference the same history object throughout the project by including this file whenever history is needed
  * 
  * History is mounted to the Router object in index.js and keeps track of user navigation throughout the app
  */
